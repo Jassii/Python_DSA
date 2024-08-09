21afed3ef2c92ac989a5fa7cfe05f3e28a28b3cf
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2024-08-09 12:17:40 +0530

    Create Sum_Of_N_Naturalnumbers.py

---
+++ Analysis Of Algorithms/Sum_Of_N_Naturalnumbers.py
@@ -0,0 +1,14 @@
+#input the number
+n=int(input())
+
+#need to get the sum till n natural numbers
+
+#First Case
+sum=0
+for i in range(1,n+1):
+    sum+=i
+print(sum)
+
+#Second Case
+value = n*(n+1)/2
+print(value)
