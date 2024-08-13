8792e3fbc7f1d937b36919b60a0362f2f1be58c5
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2024-08-13 11:14:25 +0530

    Update LCM_of_two_numbers.py

--- Mathematics/LCM_of_two_numbers.py
+++ Mathematics/LCM_of_two_numbers.py
@@ -1,10 +1,25 @@
+#naive approach
+# def lcm(a,b):
+#     maxNum=max(a,b)
+#     while(True):
+#         if(maxNum%a==0 and maxNum%b==0):
+#             return maxNum
+#         else:
+#             maxNum+=1
+
+
+#Optimized approach -> a*b = lcm(a,b) * gcd(a,b)
+def gcd(a,b):
+    if(a==0):
+        return b
+    if(a>=b):
+        return gcd(a%b,b)
+    else:
+        return gcd(b%a,a)
+        
+        
 def lcm(a,b):
-    maxNum=max(a,b)
-    while(True):
-        if(maxNum%a==0 and maxNum%b==0):
-            return maxNum
-        else:
-            maxNum+=1
+    return (a*b)//gcd(a,b)
 
 if __name__=='__main__':
     a=int(input("Enter first number : "))
