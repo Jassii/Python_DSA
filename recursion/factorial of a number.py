7f25178895f911debdb54c71d486a1e3c4b09c1c
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2024-08-25 00:51:23 +0530

    Create factorial of a number.py

---
+++ recursion/factorial of a number.py
@@ -0,0 +1,12 @@
+#recursive method
+def fact(n):
+    #base case
+    if(n==0 or n==1):
+        return 1
+    return n*fact(n-1)    
+        
+        
+if __name__=='__main__':
+    n=int(input("Enter the number : "))
+    value = fact(n)
+    print("Factorial of a number {} is {}".format(n,value))
