6ff9ee7be89ddcd88a7f542fbde9ac3059b09d16
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2024-09-04 00:14:01 +0530

    Create Count Occurence in a Sorted Array.py

---
+++ Searching/Count Occurence in a Sorted Array.py
@@ -0,0 +1,44 @@
+def countOccurence(arr,x):
+    #get its first occurence.
+    first = firstIndex(arr,x)
+    last = lastIndex(arr,x)
+    count = last-first+1
+    return count
+
+def lastIndex(arr,x):
+    start=0
+    end=len(arr)-1
+    last=-1
+    while(start<=end):
+        mid=start+(end-start)//2
+        if(arr[mid]==x):
+            last=mid
+            start=mid+1
+        elif(arr[mid]<x):
+            start=mid+1
+        else:
+            end=mid-1
+    return last
+    
+
+def firstIndex(arr,x):
+    start=0
+    end=len(arr)-1
+    first=-1
+    while(start<=end):
+        mid=start+(end-start)//2
+        if(arr[mid]==x):
+            first=mid
+            end=mid-1
+        elif(arr[mid]<x):
+            start=mid+1
+        else:
+            end=mid-1
+    return first        
+
+if __name__=='__main__':
+    l=input("Enter number space separated : ")
+    arr = [int(num) for num in l.split(" ")]
+    x=int(input("Enter the number : "))
+    count=countOccurence(arr,x)
+    print("Count of number {} is {}".format(x,count))
